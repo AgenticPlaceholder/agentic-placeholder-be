237300e6e84eb49f11b74f2c25dc6b86fb65bd19
lizgeoge1 <liz.meringeorge@gmail.com>
2025-02-06 07:32:52 +0530

    getAdDetails updated with db call

--- src/modules/operator/operator.controller.ts
+++ src/modules/operator/operator.controller.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from 'express';
-import { getOperatorDetailsbyWalletAddress } from './operator.service';
-import logger from '../../config/logger';
+import { getOperatorDetailsbyWalletAddress,  } from './operator.service';
+import { adService } from '../advertisement/ad.service';
 
 export const getOperatorDetails = async (req: Request, res: Response) => {
     try {
-        logger.info('getOperatorDetails');
         const walletAddress = req.query.walletAddress  as string;
         const operator = await getOperatorDetailsbyWalletAddress(walletAddress.toLowerCase());
         res.json({
@@ -19,25 +18,33 @@ export const getOperatorDetails = async (req: Request, res: Response) => {
 
 export const getAdDetails = async (req: Request, res: Response) => {
     try {
-       
+
+        const walletAddress = req.query.walletAddress  as string;
+        const AdInfo = await adService.getActiveAd(walletAddress.toLowerCase());
         res.json({
             success: true,
-            data : {
-                publisherInfo : {
-                    name : 'Publisher ',
-                    walletAddress : '0x180c5f2aBF35442Fb4425A1edBF3B5faDFc2208D',
-                    logo : 'https://placeholderads.s3.ap-south-1.amazonaws.com/pol.JPG',      
-                },
-                AdInfo : {
-                    adTitle : 'Ad Title',
-                    adDescription : 'Ad Description',
-                    adImage : 'https://placeholderads.s3.ap-south-1.amazonaws.com/pol.JPG',
-                    repuationScore : '4.5',
-                }   
-            }
+            data : AdInfo
         });
        
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
     }
 }
+
+/*
+  "data": {
+        "publisherInfo": {
+            "_id": "67a0197670497e1fedbeccea",
+            "name": "Publisher Owner",
+            "walletAddress": "0x180c5f2abf35442fb4425a1edbf3b5fadfc2208d",
+            "logo": "https://placeholderads.s3.ap-south-1.amazonaws.com/pol.JPG",
+            "reputationScore": 10
+        },
+        "AdInfo": {
+            "adTitle": "Ad Title",
+            "adDescription": "Ad Description",
+            "adImage": "https://placeholderads.s3.ap-south-1.amazonaws.com/pol.JPG",
+            "repuationScore": "4.5"
+        }
+    }
+        */
