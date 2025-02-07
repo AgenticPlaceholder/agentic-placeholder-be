b253c6084b4b4cc9e00f89595299fb4108ebf882
lizgeoge1 <liz.meringeorge@gmail.com>
2025-02-08 01:40:57 +0530

    publishAd API updated with changed on tokenId

--- src/modules/advertisement/ad.controller.ts
+++ src/modules/advertisement/ad.controller.ts
@@ -2,13 +2,14 @@ import { Request, Response } from 'express';
 import { adService } from './ad.service';
 import { wsService } from '../../../main';
 
+// Not being used in the current implementation, using event listeners instead
 export const publishAd = async (req: Request, res: Response) => {
     try {
 
         // Gets called from the agent when the bid is finalized. 
         // The agent will call this endpoint to submit the data to the blockchain
 
-        const { adId, publisherAddress, adTitle, adDescription, adImage, operatorAddress, moneySpent } = req.body;
+        const { adId, tokenId, publisherAddress, adTitle, adDescription, adImage, operatorAddress, moneySpent } = req.body;
         req.body.status = 'active';
         // Call the service to publish the ad
         await adService.publishAd(req.body);
@@ -16,6 +17,7 @@ export const publishAd = async (req: Request, res: Response) => {
         // Notify connected clients through WebSocket
         wsService.publishAd({
             adId,
+            tokenId,
             publisherAddress,
             adTitle,
             adDescription,
